1331f44618cd0c88eb8d709e3f0ee9470895ad9d
5ms <86279729+oms01@users.noreply.github.com>
2023-08-30 12:12:19 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 12 ms, Memory: 3312 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[100'001];
+int dp[100'001];
+int cnt;
+vector<int> path;
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++) cin>>arr[i];
+    vector<int> v;
+
+    v.push_back(arr[0]);
+    for(int i=1;i<t;i++){
+        if(v.back()<arr[i]){
+            
+            v.push_back(arr[i]);
+            cnt++;
+            dp[i] = cnt;
+        }
+        else{
+            int idx = lower_bound(v.begin(),v.end(),arr[i])-v.begin();
+            v[idx] = arr[i];
+            dp[i] = idx;
+        }
+    }
+
+        cout<<t-v.size()<<'\n';
+}
