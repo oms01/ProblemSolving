df69e624b9b80cef41bb49052147a144caa64926
5ms <86279729+oms01@users.noreply.github.com>
2023-09-02 11:08:51 +0900

    [Platinum IV] Title: 먹이사슬, Time: 248 ms, Memory: 8296 KB -BaekjoonHub

---
+++ 백준/Platinum/2532. 먹이사슬/먹이사슬.cc
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define X first 
+#define Y second
+vector<pair<int,int>> v;
+bool cmp(pair<int,int> a, pair<int,int> b){
+    return a.X<b.X || a.X==b.X && a.Y > b.Y;
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        v.push_back({b,c});
+    }
+    sort(v.begin(),v.end(),cmp);
+    v.erase(unique(v.begin(),v.end()),v.end());
+    vector<int> lis;
+    for(pair<int,int> val : v){
+        if(lis.empty() || lis.back() <= -val.Y) lis.push_back(-val.Y);
+        else *upper_bound(lis.begin(),lis.end(),-val.Y) = -val.Y;
+    }
+    cout<<lis.size()<<'\n';
+}
