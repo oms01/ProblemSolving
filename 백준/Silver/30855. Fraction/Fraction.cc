2112c0dac7500fbc6725053e06df11d0e7d1e741
5ms <86279729+oms01@users.noreply.github.com>
2023-12-04 23:59:18 +0900

    [Silver I] Title: Fraction, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/30855. Fraction/Fraction.cc
+++ 백준/Silver/30855. Fraction/Fraction.cc
@@ -3,9 +3,10 @@ using namespace std;
 #define X first 
 #define Y second
 typedef pair<string,string> pss;
+typedef pair<int,int> pii;
 int n;
 stack<pss> s;
-int x,y;
+int x,y; //분자, 분모 
 int gcd(int a,int b){
     if(a==0) return b;
     return gcd(b%a,a);
@@ -14,49 +15,34 @@ int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     cin>>n;
     for(int i=0;i<n;i++){
-        string in; cin>>in;
+        string in; 
+        cin>>in;
         if(in==")"){
-            if(s.size()<4 || s.top().X=="("){
-                cout<<-1<<'\n';
-                return 0;
-            }
-            if(s.top().X=="(" || s.top().X==")"){
-                cout<<-1<<'\n';
-                return 0;
-            }
-            pair<int,int> c = {stoi(s.top().X), stoi(s.top().Y)}; s.pop();
-            if(s.top().X=="(" || s.top().X==")"){
-                cout<<-1<<'\n';
-                return 0;
-            }
-            pair<int,int> b = {stoi(s.top().X), stoi(s.top().Y)}; s.pop();
-            if(s.top().X=="(" || s.top().X==")"){
-                cout<<-1<<'\n';
-                return 0;
-            }
-            pair<int,int> a = {stoi(s.top().X), stoi(s.top().Y)}; s.pop();
-            if(s.top().X!="("){
-                cout<<-1<<'\n';
-                return 0;
-            }
+            if(s.size()<4){ cout<<-1<<'\n'; return 0; }
+            if(s.top().X=="("||s.top().X==")"){ cout<<-1<<'\n'; return 0; }
+            pii c = {stoi(s.top().X), stoi(s.top().Y)}; s.pop();
+
+            if(s.top().X=="("||s.top().X==")"){ cout<<-1<<'\n'; return 0; }
+            pii b = {stoi(s.top().X), stoi(s.top().Y)}; s.pop();
+
+            if(s.top().X=="("||s.top().X==")"){ cout<<-1<<'\n'; return 0; }
+            pii a = {stoi(s.top().X), stoi(s.top().Y)}; s.pop();
+
+            if(s.top().X!="("){ cout<<-1<<'\n'; return 0; }
             s.pop();
-            // cout<<a.X<<'/'<<a.Y<<' '<<b.X<<'/'<<b.Y<<' '<<c.X<<'/'<<c.Y<<'\n';
 
             x = a.X*b.Y*c.X + a.Y*b.X*c.Y;
             y = a.Y*b.Y*c.X;
             int g = gcd(x,y);
             x/=g;
             y/=g;
-            // cout<<x<<'/'<<y<<'\n';
             s.push({to_string(x),to_string(y)});
         }
         else{
             s.push({in,"1"});
         }
     }
-    // cout<<s.size()<<'\n';
-    if(s.size()==1){
-        cout<<x<<' '<<y<<'\n';
-    }
+    if(s.size()==1) cout<<x<<' '<<y<<'\n';
     else cout<<-1<<'\n';
+    return 0;
 }
