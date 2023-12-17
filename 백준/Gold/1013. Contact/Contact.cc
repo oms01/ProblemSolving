8276ad71cc4c80c4e1c17abf0274d8d9d1293cf7
5ms <86279729+oms01@users.noreply.github.com>
2023-12-18 02:18:49 +0900

    [Gold V] Title: Contact, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.cc
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first 
+#define Y second
+typedef long long ll;
+typedef pair<int,int> pii;
+int t;
+string str;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    cin>>t;
+    while(t--){
+        cin>>str;
+        int len = str.length();
+        if(len==1 || len==3 || str[len-1]=='0'){
+            cout<<"NO\n";
+            continue;
+        }
+        else if(len==2){
+            cout<<(str=="01" ? "YES\n":"NO\n");
+            continue;
+        }
+        bool ans = 1;
+        for(int i=0;i<len;i++){
+            // cout<<i;
+            if(str[i]=='0'&&str[i+1]=='1') i++;
+            else if(str[i]=='1'){
+                if(i>len-4){
+                    ans = 0;
+                    break;
+                }
+                if(str[i+1]!='0'||str[i+2]!='0'){
+                    ans = 0;
+                    break;
+                }
+                while(str[++i]=='0');
+                //str[i]=='1'
+                while(str[++i]=='1' && i<len);
+                if(i==len) break;
+                //str[i-1]=='1'
+                //str[i]=='0'
+                if(str[i+1]=='0'){
+                    if(str[i-2]!='1') ans = 0;
+                    else i-=2;
+                }
+                else{
+                    i++;
+                }
+            }
+            else{
+                ans = 0;
+                break;
+            }
+
+        }
+        cout<<(ans ? "YES\n":"NO\n");
+    }
+
+
+    return 0;
+}
