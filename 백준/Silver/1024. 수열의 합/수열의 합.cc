95a6e831b451dca51c075260b0c2e29bd0f54025
5ms <86279729+oms01@users.noreply.github.com>
2023-12-18 13:14:00 +0900

    [Silver II] Title: 수열의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1024. 수열의 합/수열의 합.cc
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first 
+#define Y second
+typedef long long ll;
+typedef pair<int,int> pii;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n,l;
+    cin>>n>>l;
+    int a=0,b=0;
+    do{
+        if(a!=0 && b!=0 && a/b<=0){
+            cout<<-1<<'\n';
+            return 0;
+        }
+        a = n - ((l*(l-1))/2);
+        b = l;
+        if(a%b==0) break;
+        l++;
+        if(l>100){
+            cout<<-1<<'\n';
+            return 0;
+        }
+    }while(1);
+    int a1 = a/b;
+    if(a1<0){
+        cout<<-1<<'\n';
+        return 0;
+    }
+    for(int i=0;i<l;i++){
+        cout<<a1+i<<' ';
+    }
+}
