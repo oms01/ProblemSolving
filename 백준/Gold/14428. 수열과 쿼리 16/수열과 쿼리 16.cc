16b41665076d9611f9f2f9143d53c03e49fb5fb9
5ms <86279729+oms01@users.noreply.github.com>
2024-02-03 23:04:15 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 76 ms, Memory: 12964 KB -BaekjoonHub

--- 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.cc
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.cc
@@ -1,32 +1,36 @@
 #include<bits/stdc++.h>
-typedef long long ll;
 using namespace std;
+typedef long long ll;
+ll MX = 1'000'000'007;
 vector<ll> seg;
 ll arr[1'000'001];
-ll func(int a,int b){
+ll cmp(int a, int b){
     if(a==-1) return b;
     if(b==-1) return a;
-
     if(arr[a]==arr[b]) return min(a,b);
-    return arr[a]>arr[b] ? b:a;
+    return arr[a]>arr[b] ? b : a;
 }
 ll init(int idx, int st, int en){
     if(st==en) return seg[idx] = st;
-    return seg[idx] = func(init(2*idx,st,(st+en)/2) , init(2*idx+1,(st+en)/2+1,en));
+    int mid = (st+en)/2;
+    return seg[idx] = cmp(init(idx*2, st, mid),init(idx*2+1, mid+1, en));
 }
-ll cal(int idx, int st, int en, int l, int r){
-    if(l>en || r<st) return -1;
-    if(l<=st && en<=r) return seg[idx];
-    return func(cal(2*idx,st,(st+en)/2,l,r) , cal(2*idx+1,(st+en)/2+1,en,l,r));
-}
-ll update(int idx, int st, int en, int i, ll n){
-    if(i<st||i>en) return seg[idx];
+ll update(int idx, int st, int en, int i, ll val){
+    if(st>i||i>en) return seg[idx];
     if(st==en) return seg[idx];
-    return seg[idx] = func(update(2*idx,st,(st+en)/2,i,n) , update(2*idx+1,(st+en)/2+1,en,i,n));
+    int mid = (st+en)/2;
+    return seg[idx] = cmp(update(idx*2, st, mid, i, val),update(idx*2+1, mid+1, en, i, val));
+}
+ll query(int idx, int st, int en, int l, int r){
+    if(l>en||r<st) return -1;
+    if(l<=st && en<=r) return seg[idx];
+    int mid = (st+en)/2;
+    return cmp(query(idx*2, st, mid, l, r),query(idx*2+1, mid+1, en, l, r));
 }
 int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-    int n,m;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m,k;
     cin>>n;
     for(int i=0;i<n;i++) cin>>arr[i];
     seg.resize(4*n);
@@ -36,12 +40,12 @@ int main(){
         ll a,b,c;
         cin>>a>>b>>c;
         if(a==1){
-            arr[b-1]=c;
+            arr[b-1] = c;
             update(1,0,n-1,b-1,c);
         }
         else{
-            cout<<cal(1,0,n-1,b-1,c-1)+1<<'\n';
+            int mn = query(1,0,n-1,b-1,c-1);
+            cout<<query(1,0,n-1,b-1,c-1) + 1<<'\n';
         }
     }
-
 }
