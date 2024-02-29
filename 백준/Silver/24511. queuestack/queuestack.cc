cd4ea555800364ffce8c900603245ff75417127b
5ms <86279729+oms01@users.noreply.github.com>
2024-02-29 13:15:03 +0900

    [Silver III] Title: queuestack, Time: 44 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.cc
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first 
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+typedef long long ll;
+typedef pair<int,int> pii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin>>n;
+    vector<int> type(n);
+    deque<int> d;
+    rep(i,0,n){
+        cin>>type[i];
+    }
+    rep(i,0,n){
+        int val; cin>>val;
+        if(type[i]==0){
+            d.push_front(val);
+        }
+    }
+
+    int m; cin>>m;
+    while(m--){
+        int val; cin>>val;
+        d.push_back(val);
+        cout<<d.front()<<' '; d.pop_front();
+    }
+}
+
