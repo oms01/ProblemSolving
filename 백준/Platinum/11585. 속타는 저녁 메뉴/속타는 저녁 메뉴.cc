646d7cde112ed9c29c4f3676baa764da7dfb6b70
5ms <86279729+oms01@users.noreply.github.com>
2024-03-15 20:42:56 +0900

    [Platinum V] Title: 속타는 저녁 메뉴, Time: 60 ms, Memory: 21060 KB -BaekjoonHub

--- 백준/Platinum/11585. 속타는 저녁 메뉴/속타는 저녁 메뉴.cc
+++ 백준/Platinum/11585. 속타는 저녁 메뉴/속타는 저녁 메뉴.cc
@@ -1,58 +1,59 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-vector<int> failure;
-void fail(string pattern){
-    failure.resize(pattern.length(),0);
-    //Failure function
-    int j = 0;
-    for (int i = 1; i < pattern.size(); i++) {
-        while (j > 0 && pattern[i] != pattern[j]) j = failure[j - 1];
-        if (pattern[i] == pattern[j]) {
-            failure[i] = j + 1;
-            j++;
-        }
+#define X first 
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+typedef long long ll;
+typedef pair<int,int> pii;
+
+
+vector<int> getFail(string str){
+    int len = str.length();
+    vector<int> fail(len);
+    for(int i=1,j=0;i<len;i++){
+        while(j>0 && str[i]!=str[j]) j = fail[j-1];
+        if(str[i]==str[j]) fail[i] = ++j;
     }
+    return fail;
 }
-vector<int> func(string str, string pattern){
+
+vector<int> kmp(string str, string pattern){
     vector<int> ret;
-    //KMP function
-    int j = 0;
-    for (int i = 0; i < str.size(); i++) {
-        while (j > 0 && str[i] != pattern[j]) j = failure[j - 1];
-        if (str[i] == pattern[j]) {
-            if (j == pattern.size() - 1) {
+    vector<int> fail = getFail(pattern);
+    for(int i=0,j=0;i<str.size();i++){
+        while(j>0&&str[i]!=pattern[j]) j=fail[j-1];
+        if(str[i]==pattern[j]){
+            if(j==pattern.size()-1){
                 ret.push_back(i-pattern.length()+2);
-                j = failure[j];
+                j = fail[j];
             }
             else j++;
         }
     }
     return ret;
 }
-int gcd(int a,int b){
-    if(a==0) return b;
-    return gcd(b%a,a);
-}
+
+
+
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
-    int t;
-    cin>>t;
+    int t; cin>>t;
     string pattern="";
     for(int i=0;i<t;i++){
-        char a; cin>>a;
-        pattern+=a;
+        char c; cin>>c;
+        pattern+=c;
     }
     string str="";
     for(int i=0;i<t;i++){
-        char a; cin>>a;
-        str+=a;
+        char c; cin>>c;
+        str+=c;
     }
     str+=str;
     str = str.substr(0,str.size()-1);
-    fail(pattern);
-    vector<int> v = func(str,pattern);
+    vector<int> v = kmp(str,pattern);
     int a = v.size();
     int b = t;
-    cout<<a/gcd(a,b)<<'/'<<b/gcd(a,b)<<'\n';
+    int gcd = __gcd(a,b);
+    cout<<a/gcd<<'/'<<b/gcd<<'\n';
 
 }
