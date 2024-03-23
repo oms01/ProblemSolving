9c9532bd81d44168071b07b6d0c7394b7ad38064
5ms <86279729+oms01@users.noreply.github.com>
2024-03-23 15:15:51 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 3.15 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,44 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-pair<string,string> split(string str){
-    int idx = 0;
-    while(str[idx]!=' ') idx++;
-    return {str.substr(0,idx), str.substr(idx+1,str.length())};
-}
 int solution(vector<string> friends, vector<string> gifts) {
     int answer = 0;
     map<string,int> m;
     vector<vector<int>> v(friends.size(),vector<int>(friends.size()));
-    for(int i=0;i<friends.size();i++){
-        m[friends[i]] = m.size();
-    }
+    for(auto cur : friends) m[cur] = m.size();
     map<int,int> point;
-    for(int i=0;i<gifts.size();i++){
-        pair<string, string> cur = split(gifts[i]);
-        v[m[cur.first]][m[cur.second]]++;
-        point[m[cur.first]]++;
-        point[m[cur.second]]--;
+    for(auto cur : gifts){
+        string a,b;
+        stringstream ss(cur);
+        ss>>a>>b;
+        v[m[a]][m[b]]++;
+        point[m[a]]++;
+        point[m[b]]--;
     }
     
-    map<int,int> cnt;
+    vector<int> cnt(v.size(),0);
     for(int i=0;i<v.size();i++){
-        for(int j=i+1;j<v.size();j++){
-            int a = v[i][j];
-            int b = v[j][i];
-            if(a==b){
-                //선물지수 비교
-                if(point[i] > point[j]) cnt[i]++;
-                else if(point[i] < point[j]) cnt[j]++;
-            }
-            else if(a>b) cnt[i]++;
-            else if(a<b) cnt[j]++;
+        for(int j=0;j<v.size();j++){
+            if(i==j) continue;
+            if((v[i][j]==v[j][i]&&point[i]>point[j])||v[i][j]>v[j][i]) cnt[i]++;
         }
     }
-    
-    for(auto i=cnt.begin();i!=cnt.end();i++){
-        answer = max(answer,i->second);
-    }
-    
-    return answer;
+    return *max_element(cnt.begin(),cnt.end());
 }
