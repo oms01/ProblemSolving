ccc87d00bfd6bf0fe7f58ddd38ac37ecd621cdf5
5ms <86279729+oms01@users.noreply.github.com>
2024-03-30 13:00:30 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first 
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+typedef long long ll;
+typedef pair<int,int> pii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*------------------------------*/
+
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    string str; cin>>str;
+    int ans = 0;
+    int idx = 0;
+    while(++ans){
+        string tmp = to_string(ans);
+        for(int i=0;i<tmp.length();i++){
+            if(str[idx]==tmp[i]) idx++;
+            if(idx==str.length()){
+                cout<<ans<<'\n';
+                return 0;
+            }
+        }
+    }
+}
