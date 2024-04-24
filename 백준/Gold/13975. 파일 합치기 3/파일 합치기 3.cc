c56dd1da4142881d3371145647210572e7fad86c
5ms <86279729+oms01@users.noreply.github.com>
2024-04-24 14:04:16 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 776 ms, Memory: 18532 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first 
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+typedef long long ll;
+typedef pair<int,int> pii;
+/*----------------------*/
+
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll t; cin>>t;
+    while(t--){
+        ll n; cin>>n;
+        priority_queue<ll,vector<ll>,greater<>> pq;
+        rep(i,0,n){
+            ll val; cin>>val;
+            pq.push(val);
+        }
+        ll ans = 0;
+        while(pq.size()!=1){
+            ll a = pq.top(); pq.pop();
+            ll b = pq.top(); pq.pop();
+            pq.push(a+b);
+            ans+=a+b;
+        }
+        cout<<ans<<'\n';
+
+    }
+}
