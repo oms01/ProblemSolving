f997a37ef81df15e0279c6c4aede7108987da6d7
5ms <86279729+oms01@users.noreply.github.com>
2024-05-29 19:36:03 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 84 ms, Memory: 4956 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -13,19 +13,19 @@ int dy[] = {1,-1,0,0};
 /*----------------------*/
 
 vector<int> adj[1'001];
-int out[1'001];
+int deg[1'001];
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int n,m; cin>>n>>m;
     rep(i,0,m){
         int s,e; cin>>s>>e;
-        out[e]++;
+        deg[e]++;
         adj[s].push_back(e);
     }
     queue<int> q;
     vector<int> ans(n+1);
     rep(i,1,n+1){
-        if(out[i]) continue;
+        if(deg[i]) continue;
         ans[i] = 1;
         q.push(i);
     }
@@ -33,8 +33,8 @@ int main(){
     while(!q.empty()){
         auto cur = q.front(); q.pop();
         for(auto nxt : adj[cur]){
-            out[nxt]--;
-            if(out[nxt]) continue;
+            deg[nxt]--;
+            if(deg[nxt]) continue;
             q.push(nxt);
             ans[nxt] = ans[cur] + 1;
         }
