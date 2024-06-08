2a171216475f81d5c50998ff9a495402a03fdcc5
5ms <86279729+oms01@users.noreply.github.com>
2024-06-08 13:15:01 +0900

    [Gold V] Title: 모독, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/16678. 모독/모독.cc
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll n; cin>>n;
+    vector<ll> v(n);
+    rep(i,0,n) cin>>v[i];
+    sort(all(v));
+    ll ans = 0;
+    int idx = 1;
+    rep(i,0,v.size()){
+        if(i!=0 && v[i]-v[i-1]<=1 && v[i]<idx) continue;
+        ans += v[i] - idx++;
+    }    
+    cout<<ans<<'\n';
+
+}
