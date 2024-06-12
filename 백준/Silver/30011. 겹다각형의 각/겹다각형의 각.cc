e22fa369d90893e5df5a620cedeb2ccde766de42
5ms <86279729+oms01@users.noreply.github.com>
2024-06-13 08:22:33 +0900

    [Silver IV] Title: 겹다각형의 각, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/30011. 겹다각형의 각/겹다각형의 각.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll n; cin>>n;
+    ll ans = 0;
+    rep(i,0,n){
+        ll x; cin>>x;
+        if(!ans) ans+=180*(x-2);
+        else ans+= 180*x;
+    }
+    cout<<ans<<'\n';
+}
