79e0a0f00fb1e3f0c1bfd94462591a281420ea38
5ms <86279729+oms01@users.noreply.github.com>
2024-06-23 12:01:54 +0900

    [Gold IV] Title: Tricknology, Time: 192 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Gold/28427. Tricknology/Tricknology.cc
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int arr[1'000'001];
+int sum[1'000'001];
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    fill(arr,arr+1'000'001,1);
+    arr[1] = 0;
+    for(int i=2;i*i<=1'000'000;i++){
+        if(!arr[i]) continue;
+        for(int j=i*i;j<=1'000'000;j+=i) arr[j] = 0;
+    }
+
+    rep(i,1,1'000'001) sum[i]+=arr[i]+sum[i-1];
+
+    int t; cin>>t;
+    while(t--){
+        int s,e; cin>>s>>e;
+        cout<<sum[2*e]-sum[2*s-1]<<'\n';
+    }
+    
+}
