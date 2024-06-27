216990913fd9c1bbd3f9cc831874e43bab9521e9
5ms <86279729+oms01@users.noreply.github.com>
2024-06-28 00:05:57 +0900

    [Silver IV] Title: GCD 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9613. GCD 합/GCD 합.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        vector<int> v(n);
+        rep(i,0,n) cin>>v[i];
+        ll sum = 0;
+        rep(i,0,n) rep(j,i+1,n) sum+=__gcd(v[i],v[j]);
+        cout<<sum<<'\n';
+    }
+}
