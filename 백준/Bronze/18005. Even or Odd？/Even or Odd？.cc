b0536fa6ca82d2aef83b668f5590508d1547874b
5ms <86279729+oms01@users.noreply.github.com>
2024-06-27 23:54:30 +0900

    [Bronze III] Title: Even or Odd?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18005. Even or Odd？/Even or Odd？.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    ll n; cin>>n;
+    if(n%2) cout<<0<<'\n';
+    else{
+        if(n*(n-1)/2%2) cout<<1<<'\n';
+        else cout<<2<<'\n';
+    }
+}
