d6ae67951baf4f9552336869d2b8075caafdfdd4
5ms <86279729+oms01@users.noreply.github.com>
2024-07-01 11:38:03 +0900

    [Silver III] Title: 발머의 피크 이론, Time: 72 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.cc
+++ 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.cc
@@ -15,25 +15,17 @@ int dy[] = {1,-1,0,0};
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     ll n,m; cin>>n>>m;
-    vector<ll> v(n+m-1);
-    rep(i,m-1,n+m-1) cin>>v[i];
-
-    vector<ll> a(n+m-1);
-    ll cur = v[m-1];
-    a[m-1] = v[m-1];
-    rep(i,m,n+m-1){
-        cur += v[i];
-        cur -= v[i-m];
-        a[i] = cur;
-    }
-
-    // rep(i,m-1,n+m-1) cout<<a[i]<<' ';
-    // cout<<'\n';
+    vector<ll> v(n);
+    ll cur = 0;
     ll ans = 0;
-    cur = 0;
-    rep(i,m-1,n+m-1){
-        if(129<=a[i] && a[i]<=138) ans++;
+    rep(i,0,n){
+        cin>>v[i];
+        cur+=v[i];
+        if(i>=m) cur-=v[i-m];
+        ans += (129<=cur&&cur<=138);
+
     }
+
     cout<<ans<<'\n';
 
 }
