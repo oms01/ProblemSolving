7051c04414c2de677562edc0f2b0e62c66fee416
5ms <86279729+oms01@users.noreply.github.com>
2024-07-03 14:10:37 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 20 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -12,24 +12,21 @@ int dx[] = {0,0,1,-1};
 int dy[] = {1,-1,0,0};
 /*----------------------*/
 
+int cur[100'001];
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int n,k; cin>>n>>k;
     vector<int> v(n);
     rep(i,0,n) cin>>v[i];
-    map<int,int> m;
-
+    
+    cur[v[0]]++;
     int st=0, en=1; // m : [st, en)
-    m[v[0]]++;
 
     int ans = 0;
     while(st<en && en<n){
-        m[v[en]]++;
-        while(m[v[en]] > k){
-            m[v[st++]]--;
-        }
-        en++;
-        ans = max(ans, en-st);
+        cur[v[en]]++;
+        while(cur[v[en]] > k) cur[v[st++]]--;
+        ans = max(ans, ++en - st);
     }
     cout<<ans<<'\n';
 }
