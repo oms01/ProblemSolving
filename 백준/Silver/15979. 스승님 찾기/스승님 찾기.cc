998ae19fe000ff448d7c03c856e5972fcd261440
5ms <86279729+oms01@users.noreply.github.com>
2024-07-10 14:41:27 +0900

    [Silver II] Title: 스승님 찾기, Time: 0 ms, Memory: 2020 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Silver/15979. 스승님 찾기/스승님 찾기.cc
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int n,m; cin>>n>>m;
+    if(n==0 && m==0) cout<<0<<'\n';
+    else if(abs(__gcd(n,m))==1) cout<<1<<'\n';
+    else cout<<2<<'\n';
+}
