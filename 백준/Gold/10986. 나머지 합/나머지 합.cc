9311bdfb5e66f5abbe498710bb331712df3a83a3
5ms <86279729+oms01@users.noreply.github.com>
2024-07-10 13:09:14 +0900

    [Gold III] Title: 나머지 합, Time: 132 ms, Memory: 9844 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,25 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define X first 
+#define X first
 #define Y second
-#define rep(i,x,y) for(ll i=x;i<y;i++)
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
 typedef long long ll;
 typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
 int dx[] = {0,0,1,-1};
 int dy[] = {1,-1,0,0};
-/*------------------------------*/
+/*----------------------*/
 
 ll arr[1001];
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    ll n,m;
-    cin>>n>>m;
+    ll n,m; cin>>n>>m;
     vector<ll> v(n+1);
     rep(i,1,n+1){
         ll x; cin>>x;
         v[i] = x + v[i-1];
-    }
-    rep(i,1,n+1){
         arr[v[i]%m]++;
     }
 
