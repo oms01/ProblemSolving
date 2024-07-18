1db354ec36fb4374a8fcf8bab2bfcb8ee9e684e7
5ms <86279729+oms01@users.noreply.github.com>
2024-07-18 22:06:53 +0900

    [Bronze I] Title: 재우야 임관하자, Time: 4 ms, Memory: 13840 KB -BaekjoonHub

---
+++ 백준/Bronze/28682. 재우야 임관하자/재우야 임관하자.cc
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin>>n;
+    string s = "swimming";
+    rep(i,0,n) cout<<s<<' ';
+    cout<<'\n';
+    cout<<flush;
+
+    rep(i,0,n){
+        string str; cin>>str;
+        cout<<(str[0]=='b' ? "soccer" : "bowling")<<' ';
+    }
+}
