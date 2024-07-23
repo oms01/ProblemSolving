6fe8b7c516fb37f7a48bb294d27ac43a06479849
5ms <86279729+oms01@users.noreply.github.com>
2024-07-23 09:58:19 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.cc
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.cc
@@ -12,30 +12,21 @@ typedef tuple<int,int,int> tiii;
 int dx[] = {0,0,1,-1};
 int dy[] = {1,-1,0,0};
 /*----------------------*/
-vector<ll> f(20);
-set<ll> s;
-void ff(ll idx, ll val){
-    if(idx==20){
-        s.insert(val);
-        return;
-    }
-
-    ff(idx+1,val);
-    ff(idx+1,val+f[idx]);
-}
-
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
+    vector<ll> f(20);
     f[0] = 1;
     rep(i,1,20){
         f[i] = f[i-1]*i;
     }
-    ff(0,0);
-    s.erase(0);
 
     ll n; cin>>n;
-    cout<<(s.find(n)!=s.end() ? "YES\n":"NO\n");
-
-
+    if(n==0) cout<<"NO\n";
+    else{
+        for(int i=19;i>=0;i--){
+            if(n>=f[i]) n-=f[i];
+        }
+        cout<<(n ? "NO\n" : "YES\n");
+    }
 
 }
