774e7cc2805bdb5254e15b2d3e5a74f9f29dfe7e
5ms <86279729+oms01@users.noreply.github.com>
2024-07-24 15:07:06 +0900

    [Bronze II] Title: 근우의 다이어리 꾸미기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/16676. 근우의 다이어리 꾸미기/근우의 다이어리 꾸미기.cc
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    string str; cin>>str;
+    if(str=="0"){
+        cout<<1<<'\n';
+        return 0;
+    }
+    string tmp = "";
+    rep(i,0,str.length()) tmp+='1';
+    cout<<str.length() - (stoi(str)<stoi(tmp))<<'\n';
+}
