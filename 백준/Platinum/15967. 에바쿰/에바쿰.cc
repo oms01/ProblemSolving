d87daea96e87243641ab44fa287fd42228844ef7
5ms <86279729+oms01@users.noreply.github.com>
2024-08-23 23:02:13 +0900

    [Platinum IV] Title: 에바쿰, Time: 344 ms, Memory: 64520 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/15967. 에바쿰/에바쿰.cc
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+const int MX = 1'000'001;
+ll seg[4*MX], lazy[4*MX];
+void lazy_update(int idx, int st, int en){
+    if(!lazy[idx]) return;
+    seg[idx] +=(en-st+1)*lazy[idx];
+    if(st!=en){
+        lazy[idx*2]+= lazy[idx];
+        lazy[idx*2+1]+= lazy[idx];
+    }
+    lazy[idx] = 0;
+}
+void update_range(int idx, int st, int en, int l, int r, ll val){
+    lazy_update(idx,st,en);
+    if(en<l||r<st) return;
+    if(l<=st&&en<=r){
+        seg[idx]+=(en-st+1)*val;
+        if(st!=en){
+            lazy[idx*2]+=val;
+            lazy[idx*2+1]+=val;
+        }
+        return;
+    }
+    int mid = (st+en)/2;
+    update_range(idx*2,st,mid,l,r,val);
+    update_range(idx*2+1,mid+1,en,l,r,val);
+    seg[idx] = seg[idx*2] + seg[idx*2+1];
+}
+ll lazy_query(int idx, int st, int en, int l, int r){
+    lazy_update(idx,st,en);
+    if(en<l||r<st) return 0;
+    if(l<=st&&en<=r) return seg[idx];
+    int mid = (st+en)/2;
+    return lazy_query(idx*2,st,mid,l,r) + lazy_query(idx*2+1,mid+1,en,l,r);
+}
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n,m,l; cin>>n>>m>>l;
+    rep(i,0,n){
+        int x; cin>>x;
+        update_range(1,0,n-1,i,i,x);
+    }
+    rep(i,0,m+l){
+        int a; cin>>a;
+        if(a%2){
+            int b,c; cin>>b>>c;
+            cout<<lazy_query(1,0,n-1,b-1,c-1)<<'\n';
+        }
+        else{
+            int b,c,d; cin>>b>>c>>d;
+            update_range(1,0,n-1,b-1,c-1,d);           
+        }
+    }
+}
