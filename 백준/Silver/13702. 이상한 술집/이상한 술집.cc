eda8bad59d6a04367f450ca90f098008b9214de1
5ms <86279729+oms01@users.noreply.github.com>
2024-09-02 19:40:11 +0900

    [Silver II] Title: 이상한 술집, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.cc
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n,m; cin>>n>>m;
+    vector<int> v(n);
+    rep(i,0,n) cin>>v[i];
+
+    ll s=0,e=2e10;
+    while(s<=e){
+        ll mid = (s+e)/2;
+        ll cnt = 0;
+        rep(i,0,n) {
+            cnt += v[i]/mid;
+        }
+        if(cnt>=m){
+            s = mid+1;
+
+        }
+        else{
+            e = mid-1;
+        }
+    }
+    cout<<e<<'\n';
+
+}
