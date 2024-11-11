3018494236806992071c1aa6a87312bec919f4fe
5ms <86279729+oms01@users.noreply.github.com>
2024-11-11 20:48:16 +0900

    [Gold III] Title: 최대 유량, Time: 0 ms, Memory: 10004 KB -BaekjoonHub

--- 백준/Gold/6086. 최대 유량/최대 유량.cc
+++ 백준/Gold/6086. 최대 유량/최대 유량.cc
@@ -2,74 +2,74 @@
 using namespace std;
 #define X first
 #define Y second
-#define rep(i,x,y) for(int i=x;i<y;i++)
+#define rep(i,x,y) for(ll i=x;i<y;i++)
 #define all(x) begin(x),end(x)
 #define rall(x) rbegin(x),rend(x)
 typedef long long ll;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
 int dx[] = {0,0,1,-1};
 int dy[] = {1,-1,0,0};
 /*----------------------*/
-const int MX = 60;
-int c[MX][MX], level[MX], work[MX];
-vector<int> g[MX];
-int ctoi(char c){
-    if(c<='Z') return c-'A';
-    return c-'a'+26;
-}
-int st = ctoi('A');
-int en = ctoi('Z');
-bool bfs(){
-    memset(level, -1, sizeof(level));
-    queue<int> q;
-    q.push(st);
-    level[st] = 0;
-    while(!q.empty()){
-        auto cur = q.front(); q.pop();
-        for(int nxt : g[cur]){
-            if(level[nxt]!=-1 || c[cur][nxt]<=0) continue;
-            level[nxt] = level[cur] + 1;
-            q.push(nxt);
-        }
-    }
-    return (level[en]!=-1);
-}
-int dfs(int cur, int ret){
-    if(cur==en) return ret;
-    for(int i=work[cur]; i<g[cur].size();i++){
-        int nxt = g[cur][i];
-        if(level[nxt]!=level[cur]+1 || c[cur][nxt]<=0) continue;
-        int tmp = dfs(nxt, min(ret, c[cur][nxt]));
-        if(tmp > 0){
-            c[cur][nxt] -= tmp;
-            c[nxt][cur] += tmp;
-            return tmp;
-        }
-    }
-    return 0;
-}
 
+const int MX = 1000;
+vector<int> adj[MX];
+int c[MX][MX], f[MX][MX], d[MX];
+int maxflow(int st, int en){
+    int ret = 0;
+    while(1){
+        memset(d,-1,sizeof(d));
+        queue<int> q;
+        q.push(st);
+        bool flag = 0;
+        while(!q.empty()){
+            if(flag) break;
+            auto cur = q.front(); q.pop();
+            for(auto nxt : adj[cur]){
+                if(c[cur][nxt] - f[cur][nxt]>0 && d[nxt]==-1){
+                    q.push(nxt);
+                    d[nxt] = cur;
+                    if(nxt == en){
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        if(d[en]==-1) break;
+        int flow = 2e9;
+        
+        for(int i=en;i!=st;i=d[i]){
+            flow = min(flow, c[d[i]][i]-f[d[i]][i]);
+        }
+        for(int i=en;i!=st;i=d[i]){
+            f[d[i]][i]+=flow;
+            f[i][d[i]]-=flow;
+        }
+        ret += flow;
+    }
+    return ret;
+}
+map<string,int> m;
+int getHash(string str){
+    if(m.find(str)==m.end()) m[str] = m.size();
+    return m[str];
+}
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
-
     int n; cin>>n;
     rep(i,0,n){
-        char s,e; int x; 
-        cin>>s>>e>>x;
-        int u = ctoi(s); int v = ctoi(e);
-        c[u][v]+=x; c[v][u]+=x;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        string st, en; cin>>st>>en;
+        int s = getHash(st); int e = getHash(en);
+        adj[s].push_back(e); adj[e].push_back(s);
+        int a; cin>>a;
+        c[s][e]+=a;
+        c[e][s]+=a;
+
     }
 
-    int ans = 0;
-    while(bfs()){
-        memset(work, 0, sizeof(work));
-        int flow;
-        while(flow = dfs(st,2e9)) ans +=flow;
-    }
-    cout<<ans<<'\n';
+    cout<<maxflow(getHash("A"),getHash("Z"))<<'\n';
     
 }
