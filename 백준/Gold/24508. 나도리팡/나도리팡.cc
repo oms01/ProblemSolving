dd311be10b757b52b179e532acfcb30f9ea6cc94
5ms <86279729+oms01@users.noreply.github.com>
2024-12-26 14:48:55 +0900

    [Gold V] Title: 나도리팡, Time: 16 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/24508. 나도리팡/나도리팡.cc
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    ll n,k,t; cin>>n>>k>>t;
+    vector<ll> v(n);
+    rep(i,0,n) cin>>v[i];
+    sort(all(v));
+
+    ll st = 0;
+    ll en = v.size()-1;
+    ll cnt = 0;
+
+    while(v[en]<k && st<en){
+        ll move = min(k-v[en],v[st]);
+        cnt+=move;
+        v[en] += move;
+        v[st] -= move;
+        while(v[st]==0 && st<en) st++;
+        if(v[en]==k){
+            v[en--] = 0;
+        }
+    }
+
+    bool flag = (cnt>t) | *max_element(all(v));
+    cout<<(flag ? "NO":"YES");
+}
