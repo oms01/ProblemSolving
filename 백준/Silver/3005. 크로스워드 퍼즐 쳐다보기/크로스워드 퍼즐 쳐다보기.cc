62ce723a6c7436869e80ae50a075ca38a53f44b3
5ms <86279729+oms01@users.noreply.github.com>
2024-12-30 16:25:35 +0900

    [Silver II] Title: 크로스워드 퍼즐 쳐다보기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/3005. 크로스워드 퍼즐 쳐다보기/크로스워드 퍼즐 쳐다보기.cc
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int n,m; 
+string board[21];
+set<string> s;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    cin>>n>>m;
+    rep(i,0,n) cin>>board[i];
+
+    rep(i,0,n){
+        string cur = "";
+        rep(j,0,m){
+            if(board[i][j]=='#'){
+                if(cur.size()>1) s.insert(cur);
+                cur = "";
+            }
+            else cur+=board[i][j];
+        }
+        if(cur.size()>1) s.insert(cur);
+    }
+
+    rep(j,0,m){
+        string cur = "";
+        rep(i,0,n){
+            if(board[i][j]=='#'){
+                if(cur.size()>1) s.insert(cur);
+                cur = "";
+            }
+            else cur+=board[i][j];
+        }
+        if(cur.size()>1) s.insert(cur);
+    }
+    cout<<*s.begin()<<'\n';
+
+}
