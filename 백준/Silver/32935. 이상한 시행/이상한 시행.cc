d2bdbb0940ffd6c56da2bc88b277344a64fc85dd
5ms <86279729+oms01@users.noreply.github.com>
2025-02-03 14:36:58 +0900

    [Silver III] Title: 이상한 시행, Time: 44 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Silver/32935. 이상한 시행/이상한 시행.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+#define REP(i,x,y) for(ll i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    ll n; cin>>n;
+    vector<ll> v(n);
+    rep(i,0,n) cin>>v[i];
+
+    ll s = 0;
+    rep(i,0,n) s+=v[i];
+
+
+    cout<<max(s, -*min_element(all(v)))<<'\n';
+
+}
