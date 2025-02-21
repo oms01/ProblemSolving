35f5487f92a1e780282acd7c8abe1eed656790ff
5ms <86279729+oms01@users.noreply.github.com>
2025-02-21 19:29:15 +0900

    [Platinum IV] Title: 숫자의 신, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Platinum/1422. 숫자의 신/숫자의 신.cc
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+#define REP(i,x,y) for(ll i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n,k; cin>>n>>k;
+    vector<string> v(n);
+    rep(i,0,n) cin>>v[i];
+    
+    sort(all(v),[](string a,string b){
+        if(a.length() != b.length()) return a.length() > b.length();
+        return a+b>b+a;
+    });
+
+    while(v.size()<k){
+        v.push_back(v[0]);
+    }
+
+    sort(all(v),[](string a,string b){
+        return a+b>b+a;
+    });
+
+    for(auto x: v) cout<<x;
+}
