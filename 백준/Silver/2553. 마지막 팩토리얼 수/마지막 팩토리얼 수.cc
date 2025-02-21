fc02e6cf713cd5536075f3c2f6de3d85a286b2bd
5ms <86279729+oms01@users.noreply.github.com>
2025-02-21 13:24:33 +0900

    [Silver II] Title: 마지막 팩토리얼 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2553. 마지막 팩토리얼 수/마지막 팩토리얼 수.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(ll i=x;i<y;i++)
+#define REP(i,x,y) for(ll i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int n; cin>>n;
+    ll ans = 1;
+    REP(i,1,n){
+        ans*=i;
+        ans%=100'000'000;
+        while(ans%10==0){
+            ans/=10;
+        }
+    }
+    cout<<ans%10<<'\n';
+}
