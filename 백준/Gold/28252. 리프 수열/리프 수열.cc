860a349ab275dc0f2f9b51179f094ab22655fdb2
5ms <86279729+oms01@users.noreply.github.com>
2025-03-22 23:49:05 +0900

    [Gold II] Title: 리프 수열, Time: 20 ms, Memory: 4292 KB -BaekjoonHub

--- 백준/Gold/28252. 리프 수열/리프 수열.cc
+++ 백준/Gold/28252. 리프 수열/리프 수열.cc
@@ -19,42 +19,43 @@ int dy[] = {1,-1,0,0};
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
 
-    int n; cin>>n;
-
-    int cnt = 0;
-    vector<int> tmp;
-
     vector<pii> ans;
-    //초기 처리
-    int x; cin>>x;
-    if(n==1 && x>2) return !(cout<<-1<<'\n');
-    if(n==1 && x==2) ans.push_back({1,2});
-    rep(i,0,x) tmp.push_back(++cnt);
+
+    int n; cin>>n;
+    vector<int> v(n);
+    rep(i,0,n) cin>>v[i];
+    reverse(all(v));
+    if(v[0]>2) return !(cout<<-1<<'\n');
+
+    if(n==1 && v[0]==2){
+        return !(cout<<2<<'\n'<<1<<' '<<2<<'\n');
+    }
     
+    if(v[0]==1 && v[1]==1) return !(cout<<-1<<'\n');
+    
+    int cnt = 0;
+    //초기 처리
+    vector<int> tmp;
+    rep(i,0,v[0]) tmp.push_back(++cnt);
+
+    if(v[0]==2){
+        ans.push_back({tmp[0],tmp[1]});
+    }
+
 
     rep(i,1,n){
-        int x; cin>>x;
-        if(tmp.size() < x) return !(cout<<-1<<'\n');
+        if(tmp.size() > v[i]) return !(cout<<-1<<'\n');
 
         vector<int> node;
-        rep(j,0,x) node.push_back(++cnt);
-        rep(j,0,tmp.size()){
-            int ch = tmp[j];
-            int pa = node[min((int)node.size()-1,j)];
+        rep(j,0,v[i]) node.push_back(++cnt);
+        rep(j,0,node.size()){
+            int ch = node[j];
+            int pa = tmp[min((int)tmp.size()-1,j)];
             ans.push_back({ch,pa});
         }
 
-        if(i==n-1){
-            if(x>2) return !(cout<<-1<<'\n'); //마지막에 없어지는건 1개 또는 2개만
-            if(x==1 && tmp.size()==1){ //루트의 자식이 하나인 경우
-                return !(cout<<-1<<'\n');
-            }
-            if(x==2){
-                ans.push_back({node[0],node[1]});
-            }
-        }
-
         tmp = node;
+
     }
 
     cout<<cnt<<'\n';
