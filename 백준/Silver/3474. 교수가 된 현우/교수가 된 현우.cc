715ed40fa5a9adc2c04146b11a9d627ec20d9239
5ms <86279729+oms01@users.noreply.github.com>
2025-05-01 23:19:52 +0900

    [Silver III] Title: 교수가 된 현우, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,x,y) for(int i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin>>t;
+    while(t--){
+        ll n; cin>>n;
+        ll cnt = 0;
+        for(int i=5;i<=n;i*=5){
+            cnt += n/i;
+        }
+        cout<<cnt<<'\n';
+    }
+
+}
