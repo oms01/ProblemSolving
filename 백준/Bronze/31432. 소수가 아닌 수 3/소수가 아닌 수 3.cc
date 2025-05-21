76ee3226a933b99ff293417706ba69581086f965
5ms <86279729+oms01@users.noreply.github.com>
2025-05-21 16:32:11 +0900

    [Bronze I] Title: 소수가 아닌 수 3, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31432. 소수가 아닌 수 3/소수가 아닌 수 3.cc
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,x,y) for(int i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef tuple<int,int,int> tiii;
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin>>n;
+    bool ZERO=0, ONE=0;
+    rep(i,0,n){
+        int x; cin>>x;
+        if(x==1||x==0) return !(cout<<"YES\n"<<x<<'\n');
+        return !(cout<<"YES\n"<<x<<x<<'\n');
+    }
+    return !(cout<<"NO\n");
+}
