6f2cf1471208a5a57a55a38d3bd7c51c537ff88a
5ms <86279729+oms01@users.noreply.github.com>
2025-07-03 13:47:38 +0900

    [Silver II] Title: 회의실 배정 3, Time: 28 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/19622. 회의실 배정 3/회의실 배정 3.cc
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,x,y) for(int i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+typedef tuple<int,int,int> ti3;
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin>>n;
+    vector<int> v(n+1);
+    REP(i,1,n){
+        int s,e; cin>>s>>e>>v[i];
+    }
+    vector<int> dp(n+1,0);
+
+    dp[1] = v[1];
+    REP(i,2,n){
+        dp[i] = max(dp[i-1], dp[i-2]+v[i]);
+    }
+
+    cout<<dp[n];
+
+}
