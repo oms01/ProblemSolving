2eb4036ebf900f1309799e3c93abcc1a82120fd1
5ms <86279729+oms01@users.noreply.github.com>
2025-07-09 11:54:50 +0900

    [Silver V] Title: 또 수열 문제야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,x,y) for(int i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+typedef tuple<int,int,int> ti3;
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+
+    int n; cin>>n;
+
+    int i=1;
+    while(n--){
+        cout<<i<<' ';
+        i+=2;
+    }
+    
+} 
