ebd66c05ce7ff43f9de19588541992316d4aec6f
5ms <86279729+oms01@users.noreply.github.com>
2025-07-28 15:31:48 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.cc
+++ 백준/Gold/11758. CCW/CCW.cc
@@ -1,14 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int CCW(int x1, int y1, int x2, int y2, int x3, int y3){
-    ll ret = (x1*y2+x2*y3+x3*y1) - (x2*y1+x3*y2+x1*y3);
+struct P{
+    int x,y;
+};
+int CCW(P a, P b, P c){
+    ll ret = (b.x*(c.y-a.y))+(a.x*(b.y-c.y))+(c.x*(a.y-b.y));
     return !ret ? 0 : (ret<0 ? -1 : 1);
 }
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
 
-    int x1,y1,x2,y2,x3,y3;
-    cin>>x1>>y1>>x2>>y2>>x3>>y3;
-    cout<<CCW(x1,y1,x2,y2,x3,y3)<<'\n';
+    P a,b,c;
+    cin>>a.x>>a.y>>b.x>>b.y>>c.x>>c.y;
+    cout<<CCW(a,b,c)<<'\n';
 }
