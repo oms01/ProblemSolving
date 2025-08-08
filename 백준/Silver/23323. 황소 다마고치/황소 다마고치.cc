2df8a04f84bf1ef394a74fd3a3b2d3096a681542
5ms <86279729+oms01@users.noreply.github.com>
2025-08-08 13:04:09 +0900

    [Silver II] Title: 황소 다마고치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/23323. 황소 다마고치/황소 다마고치.cc
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,x,y) for(int i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+typedef tuple<int,int,int> ti3;
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+/*----------------------*/
+void solve(){
+    ll n,m; cin>>n>>m;
+    int d = 0;
+    while(n){
+        n>>=1;
+        d++;
+    }
+    cout<<d+m<<'\n';
+}
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin>>t;
+    while(t--) solve();
+}
