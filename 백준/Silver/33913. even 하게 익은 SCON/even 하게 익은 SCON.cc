178c2deaa657639620873fe7100967629188aba1
5ms <86279729+oms01@users.noreply.github.com>
2025-09-13 15:51:22 +0900

    [Silver I] Title: even 하게 익은 SCON, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/33913. even 하게 익은 SCON/even 하게 익은 SCON.cc
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,x,y) for(int i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+typedef tuple<int,int,int> ti3;
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+/*----------------------*/
+
+const ll MOD = 1e9+7;
+ll fast_mul(ll a, ll b) {
+    ll ret = 1;
+    while(b>0){
+        if(b&1) ret = a*ret%MOD;
+        a = a*a%MOD;
+        b>>=1;
+    }
+    return ret;
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n; cin>>n;
+    ll ans = (fast_mul(26,n) + fast_mul(22,n))%MOD;
+    ans = ans * (MOD+1)/2 %MOD;
+    cout<<ans<<'\n';
+}
