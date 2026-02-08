04bfdf1353ca1775edc09f060335e35976cddab3
5ms <86279729+oms01@users.noreply.github.com>
2026-02-08 10:02:56 +0900

    [Bronze IV] Title: 히스토그램, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define X first
+#define Y second
+#define rep(i,x,y) for(int i=x;i<y;i++)
+#define REP(i,x,y) for(int i=x;i<=y;i++)
+#define all(x) begin(x),end(x)
+#define rall(x) rbegin(x),rend(x)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pldld;
+typedef tuple<int,int,int> ti3;
+typedef tuple<ll,ll,ll> tl3;
+typedef tuple<ll,ll,ll,ll> tl4;
+typedef tuple<int,int,int,int> ti4;
+int dx[] = {0,0,1,-1};
+int dy[] = {-1,1,0,0};
+/*----------------------*/
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t; cin>>t;
+    while(t--){
+        int x; cin>>x;
+        while(x--) cout<<"=";
+        cout<<'\n';
+    }
+}
